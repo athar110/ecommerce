65cf70e63bfcef0e743832ff656bff7b131ff3e2
athar <atharraza786@gmail.com>
2023-12-17 23:22:01 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import{HeaderComponent} from './header/header.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent, NgbModule,
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
